b5d89851e27b116c2ba45a839e43fa1be4b277de
crankdm <crankdm@mail.uc.edu>
2023-11-30 12:03:29 -0500

    Migrations Folder

--- CP-Final/Program.cs
+++ CP-Final/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbContext>(options => {
+    options.UseSqlServer("Server=Envy; Database=FinalDB; Trusted_Connection=True; TrustServerCertificate=True");
+    options.UseSqlServer(b => b.MigrationsAssembly("CP-Final"));
+});
 
 var app = builder.Build();
 
