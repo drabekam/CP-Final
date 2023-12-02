f9a76354e4cd249fd8204329874e64f3d6afe326
sasidhar chilaka <97551417+chilaksr098@users.noreply.github.com>
2023-12-02 18:00:21 -0500

    Update 20231130165914_initialcreate.cs

--- CP-Final/Migrations/20231130165914_initialcreate.cs
+++ CP-Final/Migrations/20231130165914_initialcreate.cs
@@ -1,26 +1,3 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-#nullable disable
-
-namespace CP_Final.Migrations
-{
-    public partial class initialcreate : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-        }
-    }
-}
-
-------
-
-
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
